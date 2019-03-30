b043c7e5f2be4190172063ba112406afd2112c99
gertams <g.d.meeder@gmail.com>
2019-03-30 01:31:15 +0100

    Update cachematrix.R
    tidied up some unnecessary tests

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,11 @@
 
 ## This function takes a matrix as argument, used to set and get it's value. 
-#It must be a square matrix to also set and get the inverse
+#It must be a square matrix to also set and get the inverse. You can e.g. pass M <- cbind(c(-1,1),c(1.5,-1)) into makeCacheMatrix to test.
 makeCacheMatrix <- function(x = matrix()) {
    inv <- NULL
    print(x) #Check to see if Matrix is passed correctly
    set <- function(y) {
       x <<- y
-      print(y) #weg
       inv <<- NULL
    }
    get <- function() x
@@ -18,10 +17,10 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## This function checks to see if an inverse was calculated. If so, it gets the inverse from cache, if not it calculates it
+## This function checks to see if an inverse was calculated. If so, it gets the inverse from cache, if not it calculates it.
+## Pass the result of makeCacheMatrix as argument to test this function.
 cacheSolve <- function(x, ...) {
    inv <- x$getinverse()
-   #print(x) #weg
    if(!is.null(inv)) {
       message("getting cached data")
       return(inv)
